c08a10d6cf8ad97877439282259e3cb908ca5e31
Gun Khemnak <gunnn_boy_hand@hotmail.com>
2021-02-25 17:09:26 +0700

    add provider

--- client.go
+++ client.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 	"github.com/siteslave/grpc-rest-client/proto"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
 	//hosxpv3
 	urlHosxpv3 := os.Getenv("URL_HOSXPV3")
 	urlHosxpv4 := os.Getenv("URL_HOSXPV4")
 	urlHosxppcu := os.Getenv("URL_HOSXPPCU")
+	fmt.Print(urlHosxpv3, urlHosxpv4, urlHosxppcu)
 	connHosxpv3, err := grpc.Dial(urlHosxpv3, grpc.WithInsecure())
 	if err != nil {
 		panic(err)
@@ -28,7 +36,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// clientEmrHosxpv4 := proto.NewEmrServiceClient(connHosxpv4)
+	clientEmrHosxpv4 := proto.NewEmrServiceClient(connHosxpv4)
 	clientMasterHosxpv4 := proto.NewMasterServiceClient(connHosxpv4)
 
 	//hosxppcu
@@ -36,7 +44,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// clientEmrHosxpv4 := proto.NewEmrServiceClient(connHosxpv4)
+	clientEmrHosxppcu := proto.NewEmrServiceClient(connHosxpv4)
 	clientMasterHosxppcu := proto.NewMasterServiceClient(connHosxppcu)
 
 	app := fiber.New()
@@ -123,6 +131,41 @@ func main() {
 		}
 
 		res3, err3 := clientMasterHosxppcu.ClinicList(context.Background(), req)
+		if err3 != nil {
+			log.Fatalf("open stream error %v", err2)
+		}
+
+		data := append(res.Results, res2.Results...)
+		data = append(data, res3.Results...)
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"results": data,
+		})
+
+		// return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		// 	"error": err.Error(),
+		// })
+
+	})
+
+	app.Post("/screening", func(c *fiber.Ctx) error {
+		hospcode := c.FormValue("hospcode")
+		hn := c.FormValue("hn")
+		vn := c.FormValue("vn")
+
+		req := &proto.RequestPatient{Hospcode: hospcode, Hn: hn, Vn: vn}
+
+		res, err := clientEmrHosxpv3.GetScreening(context.Background(), req)
+		if err != nil {
+			log.Fatalf("open stream error %v", err)
+		}
+
+		res2, err2 := clientEmrHosxpv4.GetScreening(context.Background(), req)
+		if err2 != nil {
+			log.Fatalf("open stream error %v", err2)
+		}
+
+		res3, err3 := clientEmrHosxppcu.GetScreening(context.Background(), req)
 		if err3 != nil {
 			log.Fatalf("open stream error %v", err2)
 		}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/gofiber/fiber/v2 v2.5.0
 	github.com/golang/protobuf v1.4.3
+	github.com/joho/godotenv v1.3.0 // indirect
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
 )
